68067cf90dcadbc83975edb71f36cf606eedf9fa
cristianMarcial <cristian.marcial@upr.edu>
2024-08-30 12:03:45 -0400

    PRUEBA

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-#import telnetlib
+import telnetlib
 
-#HOST = "localhost"
-#tn = telnetlib.Telnet(HOST, "999")
+HOST = "localhost"
+tn = telnetlib.Telnet(HOST, "999")
 
-#tn.write("xd\n")
-#print(tn.read_all())
-print('xd')
+tn.write("xd\n")
+print(tn.read_all())
+
+#probad esto con puTTY pero en vez de poner en url de rumad, poned "localhost" en el puerto "999".
