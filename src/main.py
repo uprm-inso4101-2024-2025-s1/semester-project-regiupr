650e4588b7530d32d0953397d4e92590f9365fc7
cristianMarcial <cristian.marcial@upr.edu>
2024-08-30 21:02:23 -0400

    test

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
-import telnetlib
+import paramiko
+import os
 
-HOST = "127.0.0.0" #"10.34.3.62" #127.0.0.0
-tn = telnetlib.Telnet(HOST, "23")
+host = "127.0.0.1" #"10.34.3.62" #127.0.0.0
+port = 22
+username = "name"
+password = "string"
+break_point = 5
 
-tn.write("xd\n")
-print(tn.read_all())
+try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, port, username, password)
+    while break_point > 1:
+        try:
+            break_point-=1
+            cmd = input("$> ")
+            if cmd == "exit":
+                break
+            stdin, stdout, stderr = client.exec_command(cmd) # cmd_imput, cmd_output, cmd_line_err
+        except KeyboardInterrupt:
+            break
+    client.close()
+except Exception as err:
+    print(str(err))
 
 #probad esto con puTTY pero en vez de poner en url de rumad, poned "localhost" en el puerto "999".
