75f7051afd9fc5a62b6e958fe4b01a6f6ec7e061
cristianMarcial <cristian.marcial@upr.edu>
2024-08-31 12:05:34 -0400

    added

--- src/language.py
+++ src/language.py
@@ -1,12 +1,12 @@
 
-#
+# ---
 # NOTE: 
 # - The words and sentences should be stored apropietly in a csv 
 # or txt file and not loaded in a list.
 #
 # - This module should be in another folder diferent from 'src', but 
 # that will require further work with os.path
-#
+# ---
 
 txt_doc_EN = ["testing", "text"]
 txt_doc_ES = ["probando", "texto"]
