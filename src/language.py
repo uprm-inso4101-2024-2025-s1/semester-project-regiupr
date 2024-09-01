f9d912b7ced0f3f0c26a74586bd313d31751d7bb
cristianMarcial <cristian.marcial@upr.edu>
2024-08-31 23:25:15 -0400

    updated language module

--- src/language.py
+++ src/language.py
@@ -1,33 +1,23 @@
-import csv
-# ---
-# NOTE: 
-# - The words and sentences should be stored apropietly in a csv 
-# or txt file and not loaded in a list.
-#
-# - This module should be in another folder diferent from 'src', but 
-# that will require further work with os.path
-# ---
+import csv, os
 
+# This goes to the resources folder and loads the file with the lexicon being used in the interface
+csv_file_path = os.path.join(os.path.dirname(__file__), 'resources', 'language.csv')
 
+# Dictionary with other dictionaries with the text groups corresponding to each language
+languages = {} 
 
-# ENGLISH
-en_display_results = ["Sections:", "Department: ", "More Details", "Enroll"]
-en_show_details = ["Details for "]
-en_show_sections = ["Select Section for ", "Section: ", "Days: ", "Hours: ", "Available", "Not Available", "Availability: ", "Enroll"]
-en_enroll_section = ["Enroll", "Do you want to enroll in Section ", "for", "?", "Enrollment", "Enrolled in Section", "of", "Section ", "is not available for enrollment."]
-en_show_enrolled_courses = ["Enrolled Courses"]
-en_headers = ["Course Code", "Course Name", "Section", "Class Days", "Class Hours"]
-en_on_search = ["No courses found. Please make sure you are entering a valid course code."]
-en_other = ["Insert Course Code Below. Ex: ICOM4009"]
-txt_doc_EN = []
+def load_languages_from_csv(filepath):
+    # langauge of the section in the csv file being read
+    current_language = ""
 
-#SPANISH
-txt_doc_ES = []
+    with open(filepath, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == "LANG":
+                languages[row[1]] = {}
+                current_language = row[1]
+            else:
+                languages[current_language][row[0]] = row[1:]
 
-
-def selected_language(lang_id):
-    match lang_id:
-        case 1:
-            return txt_doc_EN
-        case 2:
-            return txt_doc_ES
+def selected_language(lang_name):
+    return languages[lang_name]
