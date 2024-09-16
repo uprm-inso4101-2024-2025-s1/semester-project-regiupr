81b06187170218524e8ded0d0f3f0640ca0b274e
Yamil-Serrano <yamil.serrano1@upr.edu>
2024-09-15 21:03:31 -0400

    Implement backend functionality for students, courses, sections, and program_faculty
    - Added Students, Student_Courses, Courses, and Program_Faculty classes.
    - Integrated CRUD operations for database handling.

--- src/Courses_and_Student_Management.py
+++ src/Courses_and_Student_Management.py
@@ -1,81 +1,190 @@
+from database.students import create_student, fetch_student, update_student, delete_student
+from database.student_courses import create_student_course, fetch_student_courses, update_student_course, delete_student_course
+from database.courses import create_course, fetch_course, update_course, delete_course, create_connection
+from database.sections import create_section, read_sections, update_section, delete_section, select_sections
+from database.program_faculty import create_program_faculty, fetch_program_faculty, update_program_faculty, delete_program_faculty
+
 class Student:
-    # Student Constructor
     def __init__(self, student_number, email, username, ssn, password):
-        # Private members
         self.student_number = student_number
         self.email = email
         self.username = username
         self.ssn = ssn
         self.password = password
 
-    # Getters
-    def get_student_number(self):
-        return self.student_number
+    def save_to_db(self):
+        connection = create_connection()
+        if connection:
+            create_student(connection, self.student_number, self.username, self.email, '1990-01-01', self.ssn, self.password)
+            connection.close()
 
-    def get_email(self):
-        return self.email
+    def update_in_db(self, field, value):
+        connection = create_connection()
+        if connection:
+            update_student(connection, self.student_number, field, value)
+            connection.close()
 
-    def get_username(self):
-        return self.username
+    @classmethod
+    def fetch_from_db(cls, student_number):
+        connection = create_connection()
+        if connection:
+            student_data = fetch_student(connection, student_number)
+            connection.close()
+            if student_data:
+                return cls(*student_data)
+        return None
 
-    def get_ssn(self):
-        return self.ssn
+    def delete_from_db(self):
+        connection = create_connection()
+        if connection:
+            delete_student(connection, self.student_number)
+            connection.close()
 
-    def get_password(self):
-        return self.password
+# Example of use:
+# student1 = Student("S004", "email@example.com", "new_user", "123456789", "password")
+# student1.save_to_db()
 
-    # Setters
-    def set_student_number(self, student_number):
-        self.student_number = student_number
+class Student_Courses:
+    def __init__(self, student_id, course_code, section_id, status):
+        self.student_id = student_id
+        self.course_code = course_code
+        self.section_id = section_id
+        self.status = status
 
-    def set_email(self, email):
-        self.email = email
+    def save_to_db(self):
+        connection = create_connection()
+        if connection:
+            create_student_course(connection, self.student_id, self.course_code, self.section_id, self.status)
+            connection.close()
 
-    def set_username(self, username):
-        self.username = username
+    def update_in_db(self, field, value):
+        connection = create_connection()
+        if connection:
+            update_student_course(connection, self.student_id, self.course_code, field, value)
+            connection.close()
 
-    def set_ssn(self, ssn):
-        self.ssn = ssn
+    @classmethod
+    def fetch_from_db(cls, student_id):
+        connection = create_connection()
+        if connection:
+            student_courses = fetch_student_courses(connection, student_id)
+            connection.close()
+            return [cls(*course) for course in student_courses]
+        return []
 
-    def set_password(self, password):
-        self.password = password
-        
+    def delete_from_db(self):
+        connection = create_connection()
+        if connection:
+            delete_student_course(connection, self.student_id, self.course_code)
+            connection.close()
+
+# Example of use:
+# student_course = Student_Courses("S001", "CS105", "A", "Enrolled")
+# student_course.save_to_db()
+
 class Course:
-     # Course Constructor
-    def __init__(self, course_Initials, course_Number, course_section, isAviable):
-        # Private members
-        self.course_Initials = course_Initials
-        self.course_Number = course_Number
-        self.course_section = course_section
-        self.isAviable = isAviable
-        
-    # Getters
-    def get_course_Initials(self):
-        return self.course_Initials
+    def __init__(self, course_code, course_name, description, credits, program):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.description = description
+        self.credits = credits
+        self.program = program
+        self.faculty = None
+        self.sections = []
 
-    def get_course_Number(self):
-        return self.course_Number
+    def save_to_db(self):
+        connection = create_connection()
+        if connection:
+            create_course(connection, self.course_code, self.course_name, self.description, self.credits, self.program)
+            connection.close()
 
-    def get_course_section(self):
-        return self.course_section
+    def update_in_db(self, field, value):
+        connection = create_connection()
+        if connection:
+            update_course(connection, self.course_code, field, value)
+            connection.close()
 
-    def get_isAviable(self):
-        return self.isAviable
+    @classmethod
+    def fetch_from_db(cls, course_code):
+        connection = create_connection()
+        if connection:
+            course_data = fetch_course(connection, course_code)
+            connection.close()
+            if course_data:
+                return cls(*course_data)
+        return None
 
-    # Setters
-    def set_course_Initials(self, course_Initials):
-        self.course_Initials = course_Initials
+    def delete_from_db(self):
+        connection = create_connection()
+        if connection:
+            delete_course(connection, self.course_code)
+            connection.close()
 
-    def set_course_Number(self, course_Number):
-        self.course_Number = course_Number
+    def add_section(self, section_id, professor_name, days, schedule, room, modality, capacity):
+        connection = create_connection()
+        if connection:
+            create_section(connection, section_id, self.course_code, professor_name, days, schedule, room, modality, capacity)
+            connection.close()
+            self.sections.append({
+                "section_id": section_id,
+                "professor_name": professor_name,
+                "days": days,
+                "schedule": schedule,
+                "room": room,
+                "modality": modality,
+                "capacity": capacity
+            })
 
-    def set_course_section(self, course_section):
-        self.course_section = course_section
+    def update_section(self, section_id, field, value):
+        connection = create_connection()
+        if connection:
+            update_section(connection, section_id, **{field: value})
+            connection.close()
 
-    def set_isAviable(self, isAviable):
-        self.isAviable = isAviable
+    def delete_section(self, section_id):
+        connection = create_connection()
+        if connection:
+            delete_section(connection, section_id)
+            connection.close()
+            self.sections = [sec for sec in self.sections if sec["section_id"] != section_id]
 
-# Examples of how to create an object and use its getters and setters:
-# exampleStudent1 = Student(12345, "student@example.com", "username123", "802-24-0000", "mypassword")
-# exampleStudent1.set_email("emailChanged.com")
-# print(exampleStudent1.get_email())
+    def fetch_sections(self):
+        connection = create_connection()
+        if connection:
+            self.sections = select_sections(connection, course_id=self.course_code)
+            connection.close()
+        return self.sections
+
+    def add_faculty(self, faculty):
+        connection = create_connection()
+        if connection:
+            create_program_faculty(connection, self.program, faculty)
+            connection.close()
+            self.faculty = faculty
+
+    def fetch_faculty(self):
+        connection = create_connection()
+        if connection:
+            faculty_data = fetch_program_faculty(connection, self.program)
+            connection.close()
+            if faculty_data:
+                self.faculty = faculty_data[1]
+            return self.faculty
+
+    def update_faculty(self, new_faculty):
+        connection = create_connection()
+        if connection:
+            update_program_faculty(connection, self.program, 'faculty', new_faculty)
+            connection.close()
+            self.faculty = new_faculty
+
+    def delete_faculty(self):
+        connection = create_connection()
+        if connection:
+            delete_program_faculty(connection, self.program)
+            connection.close()
+            self.faculty = None
+
+# Example of use:
+# course = Course('CIIC3015', 'Introduction to Computer Programming', 'Learning programming', 3, 'CIIC')
+# course.save_to_db()
