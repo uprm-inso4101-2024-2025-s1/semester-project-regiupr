303518c4cdfcc1a41034703f20462b5f98d1704e
cristianMarcial <cristian.marcial@upr.edu>
2024-09-17 07:43:49 -0400

    Added module for importing the student's cruds to the working directory

---
+++ src/db_test.py
@@ -0,0 +1,7 @@
+import os, sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'database', 'courses', 'courses'))
+
+#sys.path.append(os.path.dirname(__file__), 'database', 'courses', 'courses.py')
+
+import courses
