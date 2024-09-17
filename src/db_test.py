25b0a8079c12201503eccf54576f4ffc8e47937b
cristianMarcial <cristian.marcial@upr.edu>
2024-09-17 07:53:45 -0400

    fixed path errors.

--- src/db_test.py
+++ src/db_test.py
@@ -1,7 +1,7 @@
 import os, sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'database', 'courses', 'courses'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'database'))
 
-#sys.path.append(os.path.dirname(__file__), 'database', 'courses', 'courses.py')
+import database.courses.courses as cc
 
-import courses
+cc.main()
