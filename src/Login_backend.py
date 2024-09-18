c1a39af45a5f9a3de43fb501fe4c698dd52e4b19
cristianMarcial <cristian.marcial@upr.edu>
2024-09-18 08:42:05 -0400

    Changed dummy student #1 to match the example labels on the login screen and placed connection global variable outside the start_login function

--- src/Login_backend.py
+++ src/Login_backend.py
@@ -1,6 +1,9 @@
 from DB_connection import StudentsM 
 
-# This item is used by other modules with display logged student's info to know which student info has to be displayed
+# Connection to be established with the database server.
+global connection
+
+# This item is used by other modules with display logged student's info to know which student info has to be displayed.
 global students_id
 
 def verify_credentials(username, student_id, password):
@@ -16,15 +19,15 @@ def verify_credentials(username, student_id, password):
     fetched_email = StudentsM.fetch_student(connection, student_id)[2]
     fetched_password = StudentsM.fetch_student(connection, student_id)[5]
 
-    # Notice how the 3rd argument is an integer passed in this function is turned into an integer
+    # Notice how the 3rd argument is an integer passed in this function is turned into an integer.
     return (fetched_email == username and fetched_password == int(password))
 
 def start_login():
-    global connection
+    #Initialize the global variable for database server connection.
     connection = StudentsM.create_connection()
 
-    # Two dummy student's data to be used as an example
-    StudentsM.create_student(connection, "802-12-3456", "Juan Lopez", "juan.delpueblo@upr.edu", "1998-01-15", 123456700, 1234)
+    # Two dummy student's data to be used as an example.
+    StudentsM.create_student(connection, "802-12-3456", "Juan Lopez", "student.name@upr.edu", "1998-01-15", 123456700, 1234)
     StudentsM.create_student(connection, "802-21-6890", "Kiara Gonzales", "kiara.gonzales@upr.edu", "1998-01-15", 123456780, 5678)
 
 def end_session():
