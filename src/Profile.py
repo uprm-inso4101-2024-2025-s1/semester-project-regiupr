dfd1ec11969f8aee7b793aa71f8adb83445b829e
cristianMarcial <cristian.marcial@upr.edu>
2024-09-21 20:45:00 -0400

    Fixed argument on update_student_data_displayed at profile

--- src/Profile.py
+++ src/Profile.py
@@ -33,7 +33,7 @@ class Profile(QWidget):
 
         #this tests the backend by fetching the student with id 802-12-3456 as default
         #something similar needs to be done elsewhere and then have the widgets be updated with new data
-        self.update_student_data_displayed(Profile_Backend.get_student_data())
+        self.update_student_data_displayed(Login_backend.get_student_id())
 
         self.setWindowTitle("Profile Page")
         self.setGeometry(100, 100, 900, 600)
