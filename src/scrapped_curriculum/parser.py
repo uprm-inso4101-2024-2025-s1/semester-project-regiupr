aaea7e966bf5d697e78b6f64c7b7fc67b04fecec
cristianMarcial <cristian.marcial@upr.edu>
2024-10-08 08:40:55 -0400

    added things

--- src/scrapped_curriculum/parser.py
+++ src/scrapped_curriculum/parser.py
@@ -10,15 +10,20 @@ departments = {"CIIC": "https://www.uprm.edu/registrar/sections/index.php?v1=CII
                "ADMI": "https://www.uprm.edu/registrar/sections/index.php?v1=ADMI&v2=&term=2-2024&a=s&cmd1=Search",
                "ESPA": "https://www.uprm.edu/registrar/sections/index.php?v1=ESPA&v2=&term=2-2024&a=s&cmd1=Search"}
 
+# output for the export module
+section_catalog = {}
+course_catalog = {}
+
 uprm_page = get(departments["CIIC"])
 
 scrapped_page = parse_page(uprm_page.text, "html.parser")
 catalog = scrapped_page.findAll("table", attrs={"class":"section_results"})
 
-print(catalog[0])
+print(catalog[0]) # the course 
 
 # print(uprm_page.text)
 
+# we iterate only one time but we scrap the title alone and
 # for k in departments:
 #   uprm_page = get(departments[k])
 #   #token_list = parser(uprm_page.text, "html.parser")
