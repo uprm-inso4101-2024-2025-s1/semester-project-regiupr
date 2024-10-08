2e5bb81d74f0e61f74f4d2c48ced55175dea9855
PedroRodz <pedro.rodriguez46@upr.edu>
2024-10-08 01:34:58 -0400

    Add test cases to sectionsTest.py
    Added proper unit case testing using python's unittest library

--- src/database/sections/sectionsTest.py
+++ src/database/sections/sectionsTest.py
@@ -1,24 +1,102 @@
 import mysql.connector
-
+import unittest
 import sections
-# Main execution
+
+class TestSections(unittest.TestCase):
+
+        def test_create(self):
+                sections.create_section(conn, '080', 'INSO4101', 'Marko Schutz', 'MWF', '2:30p-3:20p', 'S113', 'Presential', '100')
+                section=('080', 'INSO4101', 'Marko Schutz', 'MWF', '2:30p-3:20p', 'S113', 'Presential', 100)
+                self.assertEqual(sections.select_section(conn,'080'), 
+                                section,
+                                'Create was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+        
+        def test_delete(self):
+                sections.delete_section(conn, '080')
+                self.assertEqual(sections.select_section(conn,'080'), 
+                                None,
+                                'Deletion was unsuccesful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+
+        def test_select(self):
+                sections.create_section(conn, '080', 'INSO4101', 'Marko Schutz', 'MWF', '2:30p-3:20p', 'S113', 'Presential', '100')
+                self.assertEqual(sections.select_section(conn,'080'), 
+                                        ('080', 'INSO4101', 'Marko Schutz', 'MWF', '2:30p-3:20p', 'S113', 'Presential', 100),
+                                        'Selection was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+
+        def test_update_prof(self):
+                
+                sections.update_section(conn, '080', professor_name='Fulano' )
+                self.assertEqual(sections.select_section(conn,'080')[2], 
+                                'Fulano',
+                                'Update was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+                
+        def test_update_days(self):
+                sections.update_section(conn, '080', days='TR' )
+                self.assertEqual(sections.select_section(conn,'080')[3], 
+                                'TR',
+                                'Update was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+                
+        def test_update_schedule(self):
+                sections.update_section(conn, '080', schedule='4:00-5:20' )
+                self.assertEqual(sections.select_section(conn,'080')[4], 
+                                '4:00-5:20',
+                                'Update was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+                
+        def test_update_room(self):
+                sections.update_section(conn, '080', room='S114' )
+                self.assertEqual(sections.select_section(conn,'080')[5], 
+                                'S114',
+                                'Update was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+                
+        def test_update_modality(self):
+                sections.update_section(conn, '080', modality='Online' )
+                self.assertEqual(sections.select_section(conn,'080')[6], 
+                                'Online' ,
+                                'Update was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+        
+        def test_update_capacity(self):
+                sections.update_section(conn, '080', capacity=200 )
+                self.assertEqual(sections.select_section(conn,'080')[7], 
+                                200,
+                                'Update was unsuccessful')
+                print('Current Sections:')
+                sections.read_sections(conn)
+                print('/////////////////////')
+                
+                
+
+
 if __name__ == "__main__":
     conn = sections.create_connection()
     if conn:
-        # Example usage
-        sections.create_section(conn, '080', 'INSO4101', 'Marko Schutz', 'MWF', '2:00p-3:20p', 'S113', 'Presential', '100')
-        sections.read_sections(conn)
-        sections.update_section(conn, '080', schedule='2:30p-3:50p')
-        sections.select_sections(conn,'080')
-        print("Section id:" + sections._section_id_) 
-        print("Course id:" + sections._course_id_)
-        print("Professor Name:" + sections._professor_name_)
-        print("Days:" + sections._days_)
-        print("Schedule:" + sections._schedule_)
-        print("Room:" + sections._room_)
-        print("Modality:" + sections._modality_)
-        print("Capacity:" + sections._capacity_)
-        sections.delete_section(conn, '080')
-        sections.read_sections(conn)
-        # Close the connection
-        conn.close()
+        sections.delete_section(conn,'080')
+        unittest.main()
+        sections.delete_section(conn,'080')
+        
+
+conn.close()
+
