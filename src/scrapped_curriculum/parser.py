9557c53d693faee4703cdc0d73b11e095fc03f64
cristianMarcial <cristian.marcial@upr.edu>
2024-10-22 19:25:41 -0400

    added tester for exporting and fixed bug in parser

--- src/scrapped_curriculum/parser.py
+++ src/scrapped_curriculum/parser.py
@@ -33,17 +33,16 @@ for k in departments:
         for cell in col.find_all('td'):
             row_data.append(cell.text)
 
-    # Example
+    # Example:
     # create_section(connection, section_id, course_id, professor_name, days, schedule, room, modality, capacity)
     # sections.create_section(conn, '080', 'INSO4101', 'Marko Schutz', 'MWF', '2:00p-3:20p', 'S113', 'Presential', '100')
     row_data = list(filter(None, row_data))
 
     for i in range(0, len(row_data), 7):
         course_name = row_data[i+1].rsplit("-", 1)
-        #print(course_name)
 
         modality = 'Presential'
-        if (len(course_name[1][:3])>3):
+        if (len(course_name[1][:3]) > 3):
             if course_name[1][3] == 'D':
                 modality = 'Asynchronous'
             elif course_name[1][3] == 'E':
@@ -60,38 +59,14 @@ for k in departments:
         elm = [
             course_name[1][:3], # section
             course_name[0][-8:], # course id
-            row_data[i+5], #profesor name
-            schelude[1], # days
-            schelude[0], # shcedule
+            row_data[i+5], # profesor name
+            schelude[1].strip(), # days
+            schelude[0], # schedule
             schelude[3], # room
             modality, # it speaks by itself
             50 # capacity (50 by default for now)
         ]
         section_catalog[course_name[0][-8:] + "-" + course_name[1]] = elm
-    print(section_catalog)
 
-# print(uprm_page.text)
-
-# we iterate only one time but we scrap the title alone and
-# for k in departments:
-#   uprm_page = get(departments[k])
-#   #token_list = parser(uprm_page.text, "html.parser")
-#   print()
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# outputs for the export module:
-# section_catalog = {"course4101-101": ["depto", "professor, "etc]}
-# course_catalog = {"course": ["etc"]}
-#section_catalog = {}
-#course_catalog = {}
-
-# this scraps the page
-#uprm_page = get(departments["CIIC"])
-
-#scrapped_page = BeautifulSoup(uprm_page.text, "html.parser")
-#catalog_1 = scrapped_page.findAll("table", attrs={"class":"section_results"})
-#soup2 = BeautifulSoup('\n'.join(map(str, catalog_1)))
-
-
-#print(catalog_1[0]) # the course 
-#print((catalog_1[0]))
+# for k in section_catalog:
+#    print(k, section_catalog[k])
