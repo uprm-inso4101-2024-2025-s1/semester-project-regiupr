045dba7a4e594e5ba3bf2ea5b3ca056340336aa8
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2024-10-24 00:37:03 -0400

    Updated web scraper and parser files

--- research/parsing/course_example_01/course_parser.py
+++ research/parsing/course_example_01/course_parser.py
@@ -1,11 +1,13 @@
 from bs4 import SoupStrainer as strainer, BeautifulSoup as soup
 
 class CourseParser:
+    """Parser for course."""
+
     def __init__(self):
         self.tag_spec = ''
         self.soup = ''
 
-    def set_parsing_spec(self, tag, attribute, attr_value):
+    def strainer(self, tag, attribute, attr_value):
         """Set parsing specifictation, which are obtained
         using the strainer
         """
@@ -13,16 +15,21 @@ class CourseParser:
                                  attrs={attribute: attr_value})
         return self.tag_spec
 
-    def get_table(self, page_content):
-        """
+    def get_table_from(self, page_content):
+        """Parse the provided page content with BeautifulSoup.
+        'parse_only' parameter taken directly from itself.
         """
         self.soup = soup(page_content, 'html.parser',
                          parse_only=self.tag_spec)
         return self.soup
 
-    def get_rows(self, table):
+    def get_rows_from(self, table):
         self.rows = self.soup.find_all('tr')
         return self.rows
 
-    def get_cols(self, rows):
-        pass
+    def get_cols_from(self, rows):
+        self.cols = []
+        self.data = {}
+#        for row in rows:
+            #            for col in row:
+                
